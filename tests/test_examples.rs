93e9e0284f6c4973caa413d3fe3d322ec662312c
amogh <32526674+amogh-dambal@users.noreply.github.com>
2025-04-12 13:49:21 -0700

    test: add more example-based tests

--- tests/test_examples.rs
+++ tests/test_examples.rs
@@ -1,11 +1,26 @@
 use aws_arn::ResourceName;
 use std::str::FromStr;
 
-const EXAMPLES: &str = include_str!("examples.txt");
+#[macro_use]
+mod utils;
+
+const EXAMPLES: &str = load_test_data!("examples.txt");
+const EXAMPLES_WITH_SERVICES: &str = load_test_data!("examples-with-services.txt");
 
 #[test]
 fn test_examples_from_file() {
     for (line, arn_str) in EXAMPLES.lines().enumerate() {
+        if !arn_str.starts_with("#") {
+            println!("{:0>4}: {}", line + 1, arn_str);
+            let parsed = ResourceName::from_str(arn_str);
+            println!("{:#?}", parsed);
+            assert!(parsed.is_ok());
+        } else {
+            println!("{:0>4}: IGNORE {}", line + 1, &arn_str[1..]);
+        }
+    }
+
+    for (line, arn_str) in EXAMPLES_WITH_SERVICES.lines().enumerate() {
         if !arn_str.starts_with("#") {
             println!("{:0>4}: {}", line + 1, arn_str);
             let parsed = ResourceName::from_str(arn_str);
